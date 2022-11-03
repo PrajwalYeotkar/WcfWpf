3c084bbc5cc5a3c87506f1917723387262aec8be
Invador1904 <92011983+PrajwalYeotkar@users.noreply.github.com>
2022-11-03 12:29:05 +0530

    sql files for person database

---
+++ WCFandWPF6/People.sql
@@ -0,0 +1,24 @@
+USE [PersonDatabase]
+GO
+
+/****** Object:  Table [dbo].[People]    Script Date: 11/3/2022 11:30:45 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[People](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](max) NOT NULL,
+	[Address] [nvarchar](max) NOT NULL,
+	[Email] [nvarchar](max) NOT NULL,
+	[MobileNumber] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.People] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
